ad6760341a4ae9f385dc5ed4fea394bbe0f0fd5f
Ar2306 <anandreddy2306@gamil.com>
2024-08-16 21:23:03 +0530

    fourth

--- app_server/routes/menu.js
+++ app_server/routes/menu.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrlLocations = require('../controllers/locations'); // Adjust if needed
+const ctrlLocations = require('../controllers/locations'); 
 
 router.get('/', ctrlLocations.menu);
 
